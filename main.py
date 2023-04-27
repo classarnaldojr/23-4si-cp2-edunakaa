e0232ea069e13b602d689b182bf2dde43cd945ec
edunakaa <99845134+edunakaa@users.noreply.github.com>
2023-04-27 11:02:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,60 +20,72 @@ tesoura_template_direita = cv2.imread('tesoura_direita.png', 0)
 jogada_1_anterior = None
 jogada_2_anterior = None
 
-#Funçao para fazer o template match da mao esquerda
+# Função para realizar correspondência de modelo do template na mão esquerda.
+
+
 def detect_jogada(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    res_pedra = cv2.matchTemplate(gray, pedra_template_esquerda, cv2.TM_CCOEFF_NORMED)
-    res_papel = cv2.matchTemplate(gray, papel_template_esquerda, cv2.TM_CCOEFF_NORMED)
-    res_tesoura = cv2.matchTemplate(gray, tesoura_template_esquerda, cv2.TM_CCOEFF_NORMED)
+    res_pedra = cv2.matchTemplate(
+        gray, pedra_template_esquerda, cv2.TM_CCOEFF_NORMED)
+    res_papel = cv2.matchTemplate(
+        gray, papel_template_esquerda, cv2.TM_CCOEFF_NORMED)
+    res_tesoura = cv2.matchTemplate(
+        gray, tesoura_template_esquerda, cv2.TM_CCOEFF_NORMED)
 
     max_pedra = cv2.minMaxLoc(res_pedra)[1]
     max_papel = cv2.minMaxLoc(res_papel)[1]
     max_tesoura = cv2.minMaxLoc(res_tesoura)[1]
 
-    vars_dict = {'pedra': max_pedra, 'papel': max_papel, 'tesoura': max_tesoura}
+    vars_dict = {'pedra': max_pedra,
+                 'papel': max_papel, 'tesoura': max_tesoura}
     max_var = max(vars_dict, key=vars_dict.get)
     return max_var
 
-#Funçao para fazer o template match da mao direita
+# Função para realizar correspondência de modelo do template na mão direita.
+
+
 def detect_jogada_2(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    res_pedra = cv2.matchTemplate(gray, pedra_template_direita, cv2.TM_CCOEFF_NORMED)
-    res_papel = cv2.matchTemplate(gray, papel_template_direita, cv2.TM_CCOEFF_NORMED)
-    res_tesoura = cv2.matchTemplate(gray, tesoura_template_direita, cv2.TM_CCOEFF_NORMED)
+    res_pedra = cv2.matchTemplate(
+        gray, pedra_template_direita, cv2.TM_CCOEFF_NORMED)
+    res_papel = cv2.matchTemplate(
+        gray, papel_template_direita, cv2.TM_CCOEFF_NORMED)
+    res_tesoura = cv2.matchTemplate(
+        gray, tesoura_template_direita, cv2.TM_CCOEFF_NORMED)
 
     max_pedra = cv2.minMaxLoc(res_pedra)[1]
     max_papel = cv2.minMaxLoc(res_papel)[1]
     max_tesoura = cv2.minMaxLoc(res_tesoura)[1]
 
-    vars_dict = {'pedra': max_pedra, 'papel': max_papel, 'tesoura': max_tesoura}
+    vars_dict = {'pedra': max_pedra,
+                 'papel': max_papel, 'tesoura': max_tesoura}
     max_var = max(vars_dict, key=vars_dict.get)
     return max_var
 
 
-# Funçao para comparar jagada 1 e jogada 2 e definir o vencedor
+# Uma função responsável por comparar a jogada 1 e jogada 2 e determinar qual jogador é o vencedor.
 def comparar_jogadas(jogada_1, jogada_2, placar_1, placar_2):
     result = None
     if jogada_1 == jogada_2:
         result = 0
-        
+
     elif (
-        (jogada_1 == 'pedra' and jogada_2 == 'tesoura') or 
+        (jogada_1 == 'pedra' and jogada_2 == 'tesoura') or
         (jogada_1 == 'tesoura' and jogada_2 == 'papel') or
         (jogada_1 == 'papel' and jogada_2 == 'pedra')
     ):
         placar_1 += 1
         result = -1
-        
+
     else:
         placar_2 += 1
         result = 1
-        
 
     return placar_1, placar_2, result
-    
+
+
 # Loop principal
 while True:
     ret, frame = cap.read()
@@ -83,28 +95,35 @@ while True:
     jogada_2 = detect_jogada_2(frame)
 
     if jogada_1 is not None:
-        cv2.putText(frame, jogada_1, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(frame, jogada_1, (10, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
 
     if jogada_2 is not None:
-        cv2.putText(frame, jogada_2, (500, 49), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(frame, jogada_2, (500, 49),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
 
     if jogada_1_anterior != jogada_1 or jogada_2_anterior != jogada_2:
-        placar_1, placar_2, result = comparar_jogadas(jogada_1, jogada_2, placar_1, placar_2)
+        placar_1, placar_2, result = comparar_jogadas(
+            jogada_1, jogada_2, placar_1, placar_2)
 
         jogada_1_anterior = jogada_1
         jogada_2_anterior = jogada_2
 
-    meio_tela = int (frame.shape[1] * 0.25)
+    meio_tela = int(frame.shape[1] * 0.25)
 
-#mostra quem ganhou a rodada na tela
+# Uma função que exibe na tela o vencedor da rodada.
     if result == 0:
-        cv2.putText(frame, "Empate!", (meio_tela, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(frame, "Empate!", (meio_tela, 150),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
     elif result == -1:
-        cv2.putText(frame, "Jogador Esquerda ganhou!", (meio_tela, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(frame, "Jogador Esquerda ganhou!", (meio_tela,
+                    150), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
     elif result == 1:
-        cv2.putText(frame, "Jogador Direita ganhou!", (meio_tela, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(frame, "Jogador Direita ganhou!", (meio_tela, 150),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
 
-    cv2.putText(frame, f"Placar: (Jogador da Esquerda) {placar_1} - {placar_2} (Jogador da Direita)", (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    cv2.putText(frame, f"Placar: (Jogador da Esquerda) {placar_1} - {placar_2} (Jogador da Direita)", (
+        50, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
     cv2.imshow('frame', frame)
 
